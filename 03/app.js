0cb8ba8ea03e185d7a0f3581ff1ac72c835cc5c1
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-26 20:34:51 +0200

    task 3 completed

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,32 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const newBtn = document.createElement('button')
+for(const key  in buttonSettings) {
+        const value = buttonSettings[key]
+        if(typeof value === 'object' && value !== null ) {
+            if(key == 'attr') {
+                for(const property in value) {
+                    newBtn.setAttribute(property, value[property])
+                    console.log(`${property}: ${value[property]}`)
+    
+                }
+            }
+            if(key == 'css') {
+                for(const property in value) {
+                    newBtn.style[property] = value[property]
+                    console.log(`${property}: ${value[property]}`)
+                }
+            }
+            
+        } else {
+            console.log(`${key}: ${value}`)
+            newBtn.innerText = value
+        }
+}
+
+const parent = document.querySelector('.parent-for-button')
+console.log(parent)
+parent.appendChild(newBtn)
+
