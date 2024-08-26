cf6debb73fa3c2b5da5c61b88557decbe4cdd712
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-26 21:57:49 +0200

    task 4 completed

--- 04/app.js
+++ 04/app.js
@@ -2,7 +2,30 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+    {
+        text: 'start', 
+        url: '/'
+    },
+    {
+        text: 'galeria', 
+        url: '/gallery'
+    },
+    {
+        text: 'kontakt', 
+        url: '/contact'
+    },
 ];
+
+const ulList = document.createElement('ul')
+menuItems.forEach(item => {
+    newLi = document.createElement('li')
+    newA = document.createElement('a')
+    newA.innerText = item.text
+    newA.setAttribute('href',item.url)
+    
+    newLi.appendChild(newA)
+    ulList.appendChild(newLi)
+    
+})
+const nav = document.querySelector('nav')
+nav.appendChild(ulList)
