0778836a625ebd496a7549a4b9f75005d9957552
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-26 19:45:00 +0200

    task 1 completed

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,5 @@
 console.log('DOM');
+
+const commentsItem = document.querySelector('.comments__item ', '.comments__item--newest')
+const DataInfoElements = commentsItem.querySelectorAll('[data-info]')
+console.log(`znaleziono ${DataInfoElements.length} elementów wewnątrz elementu z tagiem ${commentsItem.nodeName}`)
