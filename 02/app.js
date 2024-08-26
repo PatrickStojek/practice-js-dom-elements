bdb2433c1d14d476e0dcc9c6af5f2b7a33625db9
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-26 19:59:15 +0200

    task 2 completed

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+const allLinks = document.querySelectorAll("[data-url]")
+console.log(allLinks)
+allLinks.forEach(link => {
+    const value = link.dataset.url
+    link.setAttribute('href', value)
+})
